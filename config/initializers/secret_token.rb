9dde2b909a1c12dd721fb2410cb246609cc2c50c
Mike Mangino <mmangino@elevatedrails.com>
2013-02-08 20:18:13 -0500

    Initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusTracker::Application.config.secret_token = 'b908d1c05bdd77e67b324d2bd4b574980dbfb4bf7e21f14f030fe99c0955d764b68bf6c868dcafea7403beea46b871a772508c5e5c14a53678c71650960f85e3'
